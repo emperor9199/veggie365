22b76bbc036ece900918e11412905cd485a93aa0
vishw kadu <1511vishwkadu@gmail.com>
2021-07-21 17:43:25 +0530

    new cards and feature com addedd

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
-// import CategoryCard from "../../components/CategoryCard/CategoryCard";
-// import CabCards from "../../components/CabCards/CabCards";
 import Testinomial from "../../components/Testinomial/Testinomial";
-// import ProductCards from "../../components/ProductCards/ProductCards";
-// import OurBest from "../../components/OurBest/OurBest";
+import Features from "../../components/Features/Features";
+import StarProducts from "../../components/StarProducts/StarProducts";
 
 const HomeScreen = () => {
   return (
     <>
       <Banner />
+      <Features />
+      <StarProducts />
+      <StarProducts />
       <Testinomial />
-
-      {/* 
-      <CategoryCard />
-      <CabCards />
-      <ProductCards
-        title="Root Vegetable"
-        discount="10"
-        subtitle="Fresh Root Vegetable"
-      />
-      <ProductCards
-        title="Root Vegetable"
-        discount="10"
-        subtitle="Fresh Root Vegetable"
-      />
-      <ProductCards
-        title="Root Vegetable"
-        discount="10"
-        subtitle="Fresh Root Vegetable"
-      />
-      <OurBest />
-       */}
     </>
   );
 };
---
+++ src/components/StarProducts/StarProducts.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import StarIcon from "@material-ui/icons/Star";
+import "./StarProducts.css";
+
+function StarProducts() {
+  return (
+    <div className="starproducts_container">
+      <div className="starproducts_head_title">
+        Root Vegetable
+        <span className="starproducts_head_dic"> (10% off)</span>
+      </div>
+      <div className="starproducts_line" />
+      <div className="starproducts_card_con">
+
+        <div className="starproduct_card">
+          <div className="starproduct_img">
+            <img
+              className="starproduct_img_data"
+              src="https://media.starquik.com/catalog/product/SQ109022.jpg"
+              alt="tomato"
+            />
+            <div className="starproduct_dis_label">4%</div>
+          </div>
+          <div className="starproduct_data">
+            <div className="starproduct_rating">
+              <StarIcon style={{ color: "gold" }} />
+              <StarIcon style={{ color: "gold" }} />
+            </div>
+            <div className="starproduct_title">tomato</div>
+            <div className="starproduct_high">SQ Special | Best Price</div>
+            <div className="starproduct_price">₹50.00 Per/Kg <del className="starproduct_price_delete">MRP ₹70.00</del></div>
+          </div>
+          <div className="starproduct_btn_con">
+            <div className="starproduct_btn">ADD</div>
+          </div>
+        </div>
+
+        <div className="starproduct_card">
+          <div className="starproduct_img">
+            <img
+              className="starproduct_img_data"
+              src="https://media.starquik.com/catalog/product/SQ109006.jpg"
+              alt="tomato"
+            />
+            <div className="starproduct_dis_label">4%</div>
+          </div>
+          <div className="starproduct_data">
+            <div className="starproduct_rating">
+              <StarIcon style={{ color: "gold" }} />
+              <StarIcon style={{ color: "gold" }} />
+            </div>
+            <div className="starproduct_title">tomato</div>
+            <div className="starproduct_high">SQ Special | Best Price</div>
+            <div className="starproduct_price">₹50.00 Per/Kg <del className="starproduct_price_delete">MRP ₹70.00</del></div>
+          </div>
+          <div className="starproduct_btn_con">
+            <div className="starproduct_btn">ADD</div>
+          </div>
+        </div>
+        <div className="starproduct_card">
+          <div className="starproduct_img">
+            <img
+              className="starproduct_img_data"
+              src="https://media.starquik.com/catalog/product/SQ101373.jpg"
+              alt="tomato"
+            />
+            <div className="starproduct_dis_label">4%</div>
+          </div>
+          <div className="starproduct_data">
+            <div className="starproduct_rating">
+              <StarIcon style={{ color: "gold" }} />
+              <StarIcon style={{ color: "gold" }} />
+            </div>
+            <div className="starproduct_title">tomato</div>
+            <div className="starproduct_high">SQ Special | Best Price</div>
+            <div className="starproduct_price">₹50.00 Per/Kg <del className="starproduct_price_delete">MRP ₹70.00</del></div>
+          </div>
+          <div className="starproduct_btn_con">
+            <div className="starproduct_btn">ADD</div>
+          </div>
+        </div>
+        <div className="starproduct_card">
+          <div className="starproduct_img">
+            <img
+              className="starproduct_img_data"
+              src="https://media.starquik.com/catalog/product/SQ101374.jpg"
+              alt="tomato"
+            />
+            <div className="starproduct_dis_label">4%</div>
+          </div>
+          <div className="starproduct_data">
+            <div className="starproduct_rating">
+              <StarIcon style={{ color: "gold" }} />
+              <StarIcon style={{ color: "gold" }} />
+            </div>
+            <div className="starproduct_title">tomato</div>
+            <div className="starproduct_high">SQ Special | Best Price</div>
+            <div className="starproduct_price">₹50.00 Per/Kg <del className="starproduct_price_delete">MRP ₹70.00</del></div>
+          </div>
+          <div className="starproduct_btn_con">
+            <div className="starproduct_btn">ADD</div>
+          </div>
+        </div>
+
+        <div className="starproduct_color_card">
+            <div className="starproduct_color_card_title">Save Max!</div>
+            <div className="starproduct_color_card_subt">Handpicked deals with best Prices.</div>
+            <div className="starproduct_color_card_btn">View All</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StarProducts;
